ba262e3dacc04c7c3434dacfd44dd08539ba9cfd
junaidak93 <87436979+junaidak93@users.noreply.github.com>
2023-03-26 22:23:49 +0500

    Updated Setup Maker to include signature

--- Setup/MakeSetup.bat
+++ Setup/MakeSetup.bat
@@ -1,1 +1,5 @@
-"Inno Setup 6/ISCC.exe" setup.iss
+REM Place the Signer folder somewhere in your hard drive and use it's path as follows
+
+D:\Signer\signtool-win10x64.exe sign /t http://timestamp.comodoca.com/authenticode /f D:\Signer\Ivacy_2021.pfx /p Gaditek QuranPresenter.exe
+
+"Inno Setup 6/ISCC.exe" setup.iss "/SsignTool=D:\Signer\signtool-win10x64.exe sign /t http://timestamp.comodoca.com/authenticode /f D:\Signer\Ivacy_2021.pfx /p Gaditek $p"
